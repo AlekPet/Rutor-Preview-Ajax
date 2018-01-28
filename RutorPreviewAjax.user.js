2afd3c612a4cdbc2ff35820d2e37bef935d87853
Aleksey <alexepetrof@gmail.com>
2018-01-28 12:18:00 +0300

    Update RutorPreviewAjax.user.js

--- RutorPreviewAjax.user.js
+++ RutorPreviewAjax.user.js
@@ -20,7 +20,7 @@
 
 // ==/UserScript==
 GM_addStyle("\
-.mDiv{width: 250px;border: 3px double #FFA302;position: fixed;right: 9px;top: 316px;text-align: center;color:white;}\
+.mDiv{width: 250px;border: 3px double #FFA302;right: 9px;top: 316px;text-align: center;color:white;}\
 .mDiv_title{background-image: url(/s/i/poisk_bg.gif);background-size: 40% 100%;padding: 5px;border-bottom: 2px solid #ffea00;}\
 .mDiv_inner{overflow-y: scroll;max-height: 400px;}\
 \
